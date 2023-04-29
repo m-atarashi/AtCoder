52eade2242dce7f4f1f4c812ee57f7f7dbc408a9
matarashi <g2123002@fun.ac.jp>
2023-04-29 23:28:00 +0900

    Update abc300d.cpp

--- abc300d.cpp
+++ abc300d.cpp
@@ -11,8 +11,6 @@ int main() {
   cin >> N;
   int M = (int)sqrt(N / (2 * 2 * 3)) + 1;
   vector<bool> is_prime(M + 1, true);
-  if (M >= 0) is_prime[0] = false;
-  if (M >= 1) is_prime[1] = false;
   for (ll i = 2; i * i <= M; i++) {
     if (!is_prime[i]) continue;
     for (ll j = i * i; j <= M; j += i) {
@@ -21,21 +19,13 @@ int main() {
   }
   ll cnt = 0;
   vector<int> P;
-  for (int i = 0; i <= M; i++) {
+  for (int i = 2; i <= M; i++) {
     if (is_prime[i]) P.push_back(i);
   }
-
-  for (int i = 0; i < (int)P.size() - 2; i++) {
-    int a = P[i];
-    if (1LL * a * a * a * a * a > N) break;
-    for (int j = i + 1; j < (int)P.size() - 1; j++) {
-      int b = P[j];
-      if (1LL * a * a * b * b * b > N) break;
-      for (int l = j + 1; l < (int)P.size(); l++) {
-        int c = P[l];
-        if (1LL * a * a * b * c * c <= N) {
-          cnt++;
-        }
+  for (int i = 0; i < (int)P.size() && 1LL * P[i] * P[i] * P[i] * P[i] * P[i] <= N; i++) {
+    for (int j = i + 1; j < (int)P.size() && 1LL * P[i] * P[i] * P[j] * P[j] * P[j] <= N; j++) {
+      for (int l = j + 1; l < (int)P.size() && 1LL * P[i] * P[i] * P[j] * P[l] * P[l] <= N; l++) {
+        cnt++;
       }
     }
   }
